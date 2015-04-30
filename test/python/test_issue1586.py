7fff75bdbf633045fad512974db2b09ac5d003aa
Bernhard Vogginger <Bernhard.Vogginger@tu-dresden.de>
2015-04-30 15:41:56 +0200

    turned test script for issue #1586 into a unit test
    Change-Id: I99c7f4dba2d5693a1ec774746b4abc3cf67e6159

--- test/python/test_issue1586.py
+++ test/python/test_issue1586.py
@@ -1,45 +1,71 @@
 #!/usr/bin/python
+import unittest
 import pylogging
-pylogging.default_config(pylogging.LogLevel.INFO)
+import pyhmf as pynn
+import pymarocco
 
-def log_to_file():
-    layout = pylogging.ColorLayout(False)
-    for domain in ["ESS","hal2ess","Calibtic", "marocco"]:
-        appender = pylogging.FileAppender(layout, "{}.log".format(domain), False)
-        log = pylogging.get(domain)
-        log.addAppender(appender)
-        log.setAdditivity(False)
-        pylogging.set_loglevel(log, pylogging.LogLevel.TRACE)
+def ESS_available():
+    try:
+        marocco = pymarocco.PyMarocco()
+        marocco.backend = pymarocco.PyMarocco.ESS
+        pynn.setup(marocco=marocco)
+        pynn.run(1.)
+        return True
+    except RuntimeError as err:
+        if err.message == "ESS not available (compile with ESS)":
+            return False
+        else:
+            raise err
 
-from pyhmf import *
-from pymarocco import PyMarocco
-from pyhalbe.Coordinate import *
 
-marocco = PyMarocco()
-marocco.placement.setDefaultNeuronSize(8)
-#hicannIndices=[0,1,2,3,4,5,6,7,12,13,14,15,16,17,18,19,28,29,30,31,32,33,34,35,48,49,50,51,52,53,54,55]
-#marocco.placement.use = [HICANNGlobal(Enum(ii)) for ii in hicannIndices]
-marocco.backend = PyMarocco.ESS
-marocco.experiment_time_offset=5e-7
+@unittest.skipUnless(ESS_available(), "Test requires ESS")
+class TestIssue1586(unittest.TestCase):
+    """
+    The ESS experiment below resulted in a IndexError in ReverseMapping when 2
+    or more HICANNS are used.
 
-n_exc = 100 # Number of excitatory neurons per group
+    Note: the experiment represents the first exc. group of the synfire chain.
+    """
 
-sim_duration = 200.
+    def runTest(self):
+        pylogging.reset()
+        pylogging.default_config(pylogging.LogLevel.INFO)
 
-pp_start = 50. # start = center of pulse-packet
+        marocco = pymarocco.PyMarocco()
+        marocco.placement.setDefaultNeuronSize(8)
 
-weight_exc = 0.002 # uS weight for excitatory to excitatory connections (double than in reference paper)
+        marocco.backend = pymarocco.PyMarocco.ESS
+        marocco.experiment_time_offset=5e-7
 
-setup(
-    max_delay = 20.,
-    marocco=marocco,
-    )
+        n_exc = 100 # Number of excitatory neurons per group
 
-exc_pop = Population(n_exc, IF_cond_exp)
-exc_pop.record()
-pop_stim = Population(n_exc, SpikeSourceArray, {'spike_times':[pp_start]},label= "pop_stim")
-Projection( pop_stim, exc_pop, FixedNumberPreConnector(60, weights=weight_exc, delays=20.), target='excitatory')
+        sim_duration = 200.
 
-log_to_file()
-run(sim_duration)
-end()
+        pp_start = 50. # start = center of pulse-packet
+
+        weight_exc = 0.002 # uS weight for excitatory to excitatory connections
+                           # (double than in reference paper)
+
+        pynn.setup(
+            max_delay = 20.,
+            marocco=marocco,
+            )
+
+        # v_thresh close to v_rest to make sure there are some spikes
+        neuron_params = {
+            'v_rest': -65.,
+            'v_thresh': -62.5,
+            }
+
+        exc_pop = pynn.Population(n_exc, pynn.IF_cond_exp, neuron_params)
+        exc_pop.record()
+        pop_stim = pynn.Population(n_exc, pynn.SpikeSourceArray, {'spike_times':[pp_start]})
+        conn = pynn.FixedNumberPreConnector(60, weights=weight_exc, delays=20.)
+        pynn.Projection( pop_stim, exc_pop, conn, target='excitatory')
+
+        pynn.run(sim_duration)
+        pynn.end()
+
+
+if __name__ == '__main__':
+    unittest.main()
