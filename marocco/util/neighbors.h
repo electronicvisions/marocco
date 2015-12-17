cb037e32ecfeadc7102092fb21629fc070b49019
Johann Klähn <johann.klaehn@kip.uni-heidelberg.de>
2015-12-17 16:37:21 +0100

    wrapper for nearest neighbor search on coordinates
    Change-Id: If291695960a84bce994c3d67081a81f21a1f868a

---
+++ marocco/util/neighbors.h
@@ -0,0 +1,170 @@
+#pragma once
+
+#include <vector>
+#include <type_traits>
+#include <boost/iterator/permutation_iterator.hpp>
+
+#include <nanoflann.hpp>
+
+#include "hal/Coordinate/geometry.h"
+
+namespace marocco {
+
+/**
+ * @brief Finds nearest neighbors for halbe grid coordinates.
+ */
+template <typename Coord, typename FloatingT = float>
+class Neighbors
+{
+	struct kd_tree_adaptor_type;
+
+public:
+	typedef FloatingT value_type;
+	typedef std::vector<Coord> points_type;
+	typedef std::vector<size_t> indices_type;
+	typedef std::vector<value_type> distances_type;
+	typedef boost::permutation_iterator<typename points_type::const_iterator,
+	                                    indices_type::const_iterator> iterator_type;
+
+	/// @brief Forwarding constructor to initialize the set of all points.
+	template <typename... F>
+	explicit Neighbors(F&&... args) : points_(std::forward<F>(args)...)
+	{
+	}
+
+	void find_near(Coord const& point, size_t num_results = 0)
+	{
+		find_near(point.x(), point.y(), num_results);
+	}
+
+	void find_near(value_type x, value_type y, size_t num_results = 0)
+	{
+		if (num_results == 0) {
+			num_results = points_.size();
+		} else if (num_results > points_.size()) {
+			// nanoflann returns garbage for this case so we throw an error.
+			throw std::runtime_error("not enough points");
+		}
+
+		indices_ = indices_type(num_results);
+		squared_distances_ = distances_type(num_results);
+		value_type point[2] = {x, y};
+		kd_tree().knnSearch(point, num_results, indices_.data(), squared_distances_.data());
+	}
+
+	/// @brief Indices into #points() container in increasing distance from query point.
+	indices_type const& indices() const
+	{
+		return indices_;
+	}
+
+	/// @brief Squared distances of neighboring points.
+	distances_type const& squared_distances() const
+	{
+		return squared_distances_;
+	}
+
+	/// @brief Set of all points to be considered as neighbors.
+	points_type const& points() const
+	{
+		return points_;
+	}
+
+	/// @brief Return distance from query point for a neighbor.
+	value_type distance(iterator_type it) const
+	{
+		return std::sqrt(squared_distances_.at(std::distance(indices_.begin(), it.base())));
+	}
+
+	/// @brief Iterator to the beginning of all found neighbors.
+	iterator_type begin() const
+	{
+		return boost::make_permutation_iterator(points_.begin(), indices_.begin());
+	}
+
+	/// @brief Iterator to the end of all found neighbors.
+	iterator_type end() const
+	{
+		return boost::make_permutation_iterator(points_.begin(), indices_.end());
+	}
+
+	/// @brief Reserve storage in points container.
+	/// @param new_cap New minimal capacity of container
+	void reserve(size_t new_cap)
+	{
+		points_.reserve(new_cap);
+	}
+
+	/// @brief Add point to set of neighbors to consider.
+	/// @note This will invalidate the index and lead to its rebuilding
+	///       on the next call to #find_near().
+	void push_back(Coord const& point)
+	{
+		dirty = true;
+		points_.push_back(point);
+	}
+
+private:
+	struct kd_tree_adaptor_type
+	{
+		points_type const& points;
+
+		/// Return the number of data points.
+		inline size_t kdtree_get_point_count() const
+		{
+			return points.size();
+		}
+
+		/// Returns the squared distance between the two-dimensional vector *p1 and
+		/// the data point with index idx_p2.
+		inline value_type
+		kdtree_distance(value_type const* p1, size_t const idx_p2, size_t /*size*/) const
+		{
+			value_type const d0 = p1[0] - value_type(points[idx_p2].x());
+			value_type const d1 = p1[1] - value_type(points[idx_p2].y());
+			return d0 * d0 + d1 * d1;
+		}
+
+		/// Returns the dim'th component of the idx'th point.
+		inline value_type kdtree_get_pt(size_t const idx, int const dim) const
+		{
+			if (dim == 0) {
+				return value_type(points[idx].x());
+			} else {
+				return value_type(points[idx].y());
+			}
+		}
+
+		template <class BBOX>
+		bool kdtree_get_bbox(BBOX& /*bb*/) const
+		{
+			// Return false to default to a standard bounding box computation.
+			return false;
+		}
+	}; // kd_tree_adaptor_type
+
+	typedef nanoflann::KDTreeSingleIndexAdaptor<
+	    nanoflann::L2_Simple_Adaptor<value_type, kd_tree_adaptor_type>,
+	    kd_tree_adaptor_type,
+	    2 /* dim */
+	    > kd_tree_type;
+
+	kd_tree_type& kd_tree()
+	{
+		if (dirty) {
+			kd_tree_.buildIndex();
+			dirty = false;
+		}
+
+		return kd_tree_;
+	}
+
+	bool dirty = true;
+	points_type points_;
+	kd_tree_adaptor_type kd_tree_adaptor_{points_};
+	kd_tree_type kd_tree_{2 /*dim*/, kd_tree_adaptor_};
+	indices_type indices_;
+	distances_type squared_distances_;
+}; // Neighbors
+
+} // namespace marocco
---
+++ test/util/test-neighbors.cpp
@@ -0,0 +1,143 @@
+#include <algorithm>
+#include <array>
+#include <set>
+
+#include "marocco/util/neighbors.h"
+#include "test/common.h"
+
+#include "hal/Coordinate/iter_all.h"
+#include "hal/Coordinate/HICANN.h"
+
+using namespace HMF::Coordinate;
+
+namespace marocco {
+
+TEST(Neighbors, CanUseCopyConstructorOfContainers)
+{
+	std::array<HICANNOnWafer, 2> hicanns{{HICANNOnWafer(Enum(2)), HICANNOnWafer(Enum(3))}};
+	Neighbors<HICANNOnWafer> neighbors(hicanns.begin(), hicanns.end());
+	ASSERT_EQ(2, neighbors.points().size());
+	ASSERT_TRUE(std::equal(hicanns.begin(), hicanns.end(), neighbors.points().begin()));
+}
+
+class NeighborsWithHICANNs : public ::testing::Test
+{
+public:
+	NeighborsWithHICANNs() : neighbors()
+	{
+	}
+
+	void add_all_hicanns()
+	{
+		neighbors.reserve(HICANNOnWafer::enum_type::size);
+		for (auto hicann : iter_all<HICANNOnWafer>()) {
+			neighbors.push_back(hicann);
+		}
+	}
+
+	static std::set<HICANNOnWafer> closest_hicanns_to(HICANNOnWafer const& hicann)
+	{
+		std::set<HICANNOnWafer> closest;
+		for (int x_offset = -1; x_offset <= 1; ++x_offset) {
+			for (int y_offset = -1; y_offset <= 1; ++y_offset) {
+				closest.insert(HICANNOnWafer(X(hicann.x() + x_offset), Y(hicann.y() + y_offset)));
+			}
+		}
+		return closest;
+	}
+
+	void expect_single_result(HICANNOnWafer const& hicann) const
+	{
+		EXPECT_EQ(1, neighbors.indices().size());
+		EXPECT_EQ(1, neighbors.squared_distances().size());
+		EXPECT_NE(neighbors.begin(), neighbors.end());
+		ASSERT_EQ(hicann, *neighbors.begin());
+	}
+
+	Neighbors<HICANNOnWafer> neighbors;
+};
+
+TEST_F(NeighborsWithHICANNs, CanReserveSpace)
+{
+	neighbors.reserve(5);
+}
+
+TEST_F(NeighborsWithHICANNs, CanBePushedTo)
+{
+	ASSERT_EQ(0, neighbors.points().size());
+	neighbors.push_back(HICANNOnWafer(Enum(2)));
+	ASSERT_EQ(1, neighbors.points().size());
+}
+
+TEST_F(NeighborsWithHICANNs, ChecksNumberOfPoints)
+{
+	HICANNOnWafer hicann{X(12), Y(13)};
+	ASSERT_ANY_THROW(neighbors.find_near(hicann, 1));
+	neighbors.push_back(hicann);
+	ASSERT_NO_THROW(neighbors.find_near(hicann, 1));
+}
+
+TEST_F(NeighborsWithHICANNs, CanFindSingleCoordinate)
+{
+	HICANNOnWafer hicann{X(12), Y(13)};
+	ASSERT_ANY_THROW(neighbors.find_near(hicann, 1));
+
+	neighbors.push_back(hicann);
+
+	neighbors.find_near(hicann, 1);
+	// Should be a perfect match for itself.
+	expect_single_result(hicann);
+}
+
+TEST_F(NeighborsWithHICANNs, CanFindNeighborsOfCoordinate)
+{
+	add_all_hicanns();
+	HICANNOnWafer hicann{X(12), Y(10)};
+
+	neighbors.find_near(hicann, 1);
+	// Should be a perfect match for itself.
+	expect_single_result(hicann);
+
+	neighbors.find_near(hicann, 9);
+	EXPECT_EQ(9, neighbors.indices().size());
+	EXPECT_EQ(9, neighbors.squared_distances().size());
+	auto closest = closest_hicanns_to(hicann);
+	std::set<HICANNOnWafer> found(neighbors.begin(), neighbors.end());
+	ASSERT_EQ(closest, found);
+}
+
+TEST_F(NeighborsWithHICANNs, CanFindNeighborsOfPoint)
+{
+	add_all_hicanns();
+
+	HICANNOnWafer hicann{X(12), Y(10)};
+	float xx = float(hicann.x()) + 0.1;
+	float yy = float(hicann.y()) + 0.1;
+
+	neighbors.find_near(xx, yy, 1);
+	expect_single_result(hicann);
+
+	neighbors.find_near(xx, yy, 9);
+	EXPECT_EQ(9, neighbors.indices().size());
+	EXPECT_EQ(9, neighbors.squared_distances().size());
+	auto closest = closest_hicanns_to(hicann);
+	std::set<HICANNOnWafer> found(neighbors.begin(), neighbors.end());
+	ASSERT_EQ(closest, found);
+}
+
+TEST_F(NeighborsWithHICANNs, ProvidesDistance)
+{
+	add_all_hicanns();
+	HICANNOnWafer hicann{X(12), Y(10)};
+
+	neighbors.find_near(hicann, 1);
+	auto it = neighbors.begin();
+	ASSERT_FLOAT_EQ(0.0, neighbors.distance(it));
+
+	neighbors.find_near(hicann, 9);
+	for (auto it = neighbors.begin(), eit = neighbors.end(); it != eit; ++it) {
+		EXPECT_LE(neighbors.distance(it), std::sqrt(2.0)) << "for " << *it;
+	}
+}
+
+} // namespace marocco
