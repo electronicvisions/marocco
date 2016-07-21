400c847c0f2462ca314522fae17bdf16b43e78b3
Johann Klähn <johann.klaehn@kip.uni-heidelberg.de>
2016-07-21 11:49:13 +0200

    Change definition of custom error types to be py++ compatible
    Change-Id: I3583165ea0a7fb7c356c6a3518a4b9d45547bcb7

--- marocco/util.h
+++ marocco/util.h
@@ -1,16 +1,29 @@
 #pragma once
+
 #include <stdexcept>
 
 namespace marocco {
 
+#ifndef PYPLUSPLUS
 #define NEW_EXCEPTION_TYPE(NAME, BASE)                                                             \
 	struct NAME : public BASE                                                                      \
 	{                                                                                              \
-		template <typename... F>                                                                   \
-		NAME(F&&... args) : BASE(std::forward<F>(args)...)                                         \
+		NAME(std::string const& what) : BASE(what)                                                 \
+		{                                                                                          \
+		}                                                                                          \
+		NAME(char const* what) : BASE(what)                                                        \
 		{                                                                                          \
 		}                                                                                          \
 	};
+#else
+#define NEW_EXCEPTION_TYPE(NAME, BASE)                                                             \
+	struct NAME : public BASE                                                                      \
+	{                                                                                              \
+		NAME(std::string const& what) : BASE(what)                                                 \
+		{                                                                                          \
+		}                                                                                          \
+	};
+#endif // !PYPLUSPLUS
 
 NEW_EXCEPTION_TYPE(Error, std::runtime_error)
 NEW_EXCEPTION_TYPE(ResourceInUseError, Error)
