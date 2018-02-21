6af3539e10280469dab9f18d5086c6668e020a55
Sebastian Schmitt <sebastian.schmitt@kip.uni-heidelberg.de>
2018-02-21 09:48:25 +0100

    Adapt synapse loss script to API changes
    Change-Id: Ibb414c198e4b2625545dbc078ed8769c2e930357

--- tools/synapse_loss.py
+++ tools/synapse_loss.py
@@ -63,7 +63,8 @@ def main():
     """
     marocco = PyMarocco()
     marocco.neuron_placement.default_neuron_size(4)
-    marocco.routing.syndriver_chain_length = 5
+    marocco.synapse_routing.driver_chain_length(5)
+    marocco.continue_despite_synapse_loss = True
 
     pynn.setup(marocco=marocco)
 
